2956d9247a0b5c051f59bc31c2317f44b465ec41
lecjxx <leticiacorreia2500@gmail.com>
2022-07-19 20:38:46 -0300

    finalizado exercícios 1, 2 e 3!

---
+++ exercicio2/script.js
@@ -0,0 +1,24 @@
+/*let pergunta1 = "Você gosta de pão?"
+let resposta1 = prompt(resposta1);
+
+let pergunta2 = "Qual sua cor favorita?"
+let resposta2 = prompt(resposta2);
+
+let pergunta3 = "Quantos anos você tem?"
+let resposta3 = prompt(resposta3);
+
+console.log (pergunta1, resposta1);
+console.log (pergunta2, resposta2);
+console.log (pergunta3, resposta3);*/
+
+const pergunta1 = "Você gosta de pão?"
+const pergunta2 = "qual sua cor favorita?"
+const pergunta3 = "Qual sua idade?"
+
+const resposta1 = prompt (pergunta1)
+const resposta2 = prompt (pergunta2)
+const resposta3 = prompt (pergunta3)
+
+console.log (pergunta1, resposta1)
+console.log (pergunta2, resposta2)
+console.log (pergunta3, resposta3)
---
+++ exercicio3/script.js
@@ -0,0 +1,9 @@
+/*let a = 10
+let b = 25
+let c = a 
+let d = a=b
+let e = b=c
+
+
+console.log("O novo valor de a é", d) // O novo valor de a é 25
+console.log("O novo valor de b é", e) // O novo valor de b é 10*/

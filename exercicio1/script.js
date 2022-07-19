aae0db3efc8b0700d7a1b7b78903f6d34b5b174d
DavidAlves28 <dav.alvesc28@gmail.com>
2022-07-19 17:49:17 -0300

    barbosa-B-David-Alves-Costa

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+// const name = ""
+// let idade =  
+//console.log(typeof name);
+//console.log(typeof idade);
+
+const name = prompt("Qual é seu nome ?");
+let idade = prompt ("Qual sua idade ?");
+
+console.log("Olá", name, "você tem",idade,"anos.")
---
+++ exercicio2/script.js
@@ -0,0 +1,12 @@
+const perguntaA = prompt ("Ola, você está bem hoje?");
+const respostaA = console.log("Ola, você está bem hoje?-", perguntaA);
+
+const perguntaB = prompt ("Está chovendo hoje?");
+const respostaB = console.log("Está chovendo hoje?-",perguntaB);
+
+const perguntaC = prompt ("Você tem carro ?");
+const respostaC = console.log("Você tem carro ?-",perguntaC);
+
+
+
+

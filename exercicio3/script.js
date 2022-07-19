202f392fdff6e0174b25c1a825bf95dc30a5e1bc
MathMendesReis <matheusmendesreis@outlook.com>
2022-07-18 23:16:39 -0300

    barbosa-matheus-mendes

---
+++ exercicio3/script.js
@@ -0,0 +1,11 @@
+let a = 10
+let b = 25
+let c;
+
+c = a
+a = b
+b = c
+
+console.log("O novo valor de a é", a) // O novo valor de a é 25
+console.log("O novo valor de b é", b) // O novo valor de b é 10
+

35472cb9ce0253bc5e16966bd635a7ec20637f2e
Astastroth <raphaelaloisiodiniz@gmail.com>
2022-07-19 16:38:42 -0300

    exercicio concluido

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+nome= prompt ('qual o seu nome?')
+idade= Number (prompt ('qual sua idade?'))
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+console.log("Olá" , nome , "você tem" , idade , "anos")
---
+++ exercicio2/script.js
@@ -0,0 +1,12 @@
+const perguntaA = "Você gosta de treinar?";
+const respostaA = prompt(perguntaA);
+
+const perguntaB = "Tem um treino adequado?";
+const respostaB = prompt(perguntaB);
+
+const perguntaC = "Está seguindo a dieta?";
+const respostaC = prompt(perguntaC);
+
+console.log('Você gosta de treinar? -' , respostaA);
+console.log("Tem um treino adequado? -", respostaB);
+console.log("Está seguindo a dieta? -", respostaC);

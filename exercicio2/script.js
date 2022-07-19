52eb56a026eaac81404dfa0ab052ab75b9d6f398
Ruan <ruanemidio1412@gmail.com>
2022-07-19 02:36:15 -0300

    Exercicio muito dificil, e tem a logica das variaveis A,B,C da aula, e o duzão novamente salvou

---
+++ exercicio2/script.js
@@ -0,0 +1,27 @@
+let resposta1 = prompt("Você está Feliz? ")
+let resposta2 = prompt("Você está gostando do curso da labenu? ")
+let resposta3 = prompt("Teve um bom dia hoje ?")
+
+//let respostaA = resposta1
+//let respostaB = resposta2
+//let respostaC = resposta3
+
+console.log(resposta1, resposta2, resposta3)
+
+let perguntaA = "Você está Feliz ?"
+let perguntaB = "Você está gostando do curso da labenu ?"
+let perguntaC = "Teve um bom dia hoje ?"
+
+console.log(perguntaA, resposta1)
+console.log(perguntaB, resposta2)
+console.log(perguntaC, resposta3)
+
+
+
+//A=1
+//B=2
+//C
+
+//C=A  // C=1, A=1, B=2
+//A=B  // B=2, A=2 C=1
+//B=C  // B=1, A=2 C=1

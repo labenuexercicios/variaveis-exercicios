9bbdce668d3274fea71b129b6185fbec06e116b1
Bruno Leite da Silva <bruno12102014@gmail.com>
2022-07-19 19:27:48 -0300

    exercico aula 007

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+const nome=prompt('Qual seu nome?');
+let idade=Number(prompt('Qual sua idade?'));
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+console.log('Olá',nome,'você tem',idade,'anos.');
+console.log(nome,idade);
+
+//Linha 1 = String = se trata de uma variavel de texto 
+//Linha 2 = Numero = se trata de uma variavel de numero
+//linha 7 = junção das variaveis
---
+++ exercicio2/script.js
@@ -0,0 +1,21 @@
+const videoGame ='Você joga video game?';
+let respostaGame = prompt(videoGame);
+
+const skate = 'Você anda de skate?';
+let respostaSkate = prompt(skate);
+
+const futebol = 'Você gosta de futebol'
+let respostaFut = prompt(futebol)
+
+console.log(videoGame + respostaGame);
+console.log(skate + respostaSkate);
+console.log(futebol + respostaFut)
+
+
+
+
+
+
+
+
+

e1e3ec844db7731955840c1ca8a485a2f46dd994
Rodolpho <nikersonrodolpho@gmail.com>
2022-07-19 20:54:31 -0300

    Aula 7 - Versao Final

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Ex 1
 
-const name  = '';
-let idade = '';
+let name
+let idade
 console.log(typeof name, typeof idade)
 
 // Como o 'vazio' está entre aspas, o valor de string (de texto) foi impresso.
 
-console.log('Qual é o seu nome?')
-console.log('Qual é a sua idade?')
+name = prompt("Qual é o seu nome?")
+idade = prompt("Qual é a sua idade?")
 
 console.log(typeof name, typeof idade)
 
@@ -17,13 +17,13 @@ console.log('Olá', name, 'você tem', idade, 'anos')
 
 const pressao_P = prompt("Você tem pressão alta?");
 const pressao_R = prompt(pressao_P)
-const pressao_PR = prompt("Você tem pressão alta? -", pressao_P);
+const pressao_PR = prompt("Você tem pressão alta? -", pressao_R);
 var mais30_P = prompt("Você tem mais de 30 anos?");
 var mais30_R = prompt(mais30_P);
-var mais30_PR = prompt("Você tem mais de 30 anos? -", mais30_P);
+var mais30_PR = prompt("Você tem mais de 30 anos? -", mais30_R);
 let cirurgia_P = prompt("Você já fez alguma cirurgia?");
 let cirurgia_R = prompt(cirurgia_P);
-let cirurgia_PR = prompt("Você já fez alguma cirurgia? -", cirurgia_P);
+let cirurgia_PR = prompt("Você já fez alguma cirurgia? -", cirurgia_R);
 
 // Ex 3
 

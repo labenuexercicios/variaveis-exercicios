de3897a04954158f50e4f448a9756844ba6f25ec
Lilian Almeida <lili.l2002@hotmail.com>
2022-07-19 16:21:15 -0300

    2 done

---
+++ exercicio2/script.js
@@ -0,0 +1,18 @@
+// let filmes = prompt("Você gosta de assistir filmes?")
+// let jogar = prompt("Você gosta de jogar?")
+// let morar = prompt("Você mora fora do brasil?")
+
+// console.log(filmes);
+// console.log(jogar);
+// console.log(morar);
+
+const perguntaA = "Você gosta de assistir filmes?"
+const respostaA = prompt(perguntaA)
+const perguntaB = "Você gosta de jogar?"
+const respostaB = prompt(perguntaB)
+const perguntaC = "Você mora fora do brasil?"
+const respostaC = prompt(perguntaC)
+
+console.log(perguntaA, "-", respostaA);
+console.log(perguntaB, "-", respostaB);
+console.log(perguntaC, "-", respostaC);

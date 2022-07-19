2a2e61b7eb99ea729349f671fb86d5aa7ea296b7
Isaías Oliveira dos Reis <isaiasodr@gmail.com>
2022-07-19 13:06:42 -0300

    exercícios finalizados

---
+++ exercicio2/script.js
@@ -0,0 +1,28 @@
+/* const Pergunta1 = prompt("Declarar variável como constante seu valor não pode ser mudado?")
+const Pergunta2 = prompt("o retorno do prompt é sempre string?")
+const Pergunta3 = prompt("A sintaxe para saber o tipo da variável é console.log(typeof.variável)?")
+
+let resposta1 = Pergunta1
+let resposta2 = Pergunta2
+let resposta3 = Pergunta3
+
+console.log(resposta1);
+console.log(resposta2);
+console.log(resposta3);  */
+
+const Pergunta1 = "Declarar variável como constante seu valor não pode ser mudado?"
+const Pergunta2 = "o retorno do prompt é sempre string?"
+const Pergunta3 = "A sintaxe para saber o tipo da variável é console.log(typeof.variável)?"
+
+const Resposta1 = prompt(Pergunta1)
+const Resposta2 = prompt(Pergunta2)
+const Resposta3 = prompt(Pergunta3)
+
+console.log(Pergunta1, "-",Resposta1)
+console.log(Pergunta2, "-",Resposta2)
+console.log(Pergunta3, "-",Resposta3)
+
+
+
+
+

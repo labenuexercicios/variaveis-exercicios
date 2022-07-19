b4c20ac803a49aaac7149e8353b7153f01428712
Josy Barros <jlsbarros@outlook.com>
2022-07-19 18:18:55 -0300

    feito exercicio1

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -6,9 +6,6 @@ let idade = prompt("Qual a sua idade?");
 
 console.log("Olá, o seu nome é", nome, "você tem", idade, "anos")
 
-
-
-
 console.log(typeof nome); // string prq tem aspas.
 console.log(typeof idade); //undefined prq representa a falta de um valor na variavel let.
 // apos utilizar a prompt as variaveis se tornaram do tipo string, isso ocorre prq o prompt

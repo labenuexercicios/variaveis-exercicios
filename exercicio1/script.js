c7261811429352966f539eaee0a3ab5b316671c3
srheizenberg <cleitonfreelance@gmail.com>
2022-07-18 23:55:22 -0300

    exercicio1 feito

---
+++ exercicio1/script.js
@@ -0,0 +1,21 @@
+let nome = ("Roberto")
+let idade = 35
+
+console.log(nome)
+console.log(idade)
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+//Você usar o typeof para saber o tipo de dados de uma variável se ela e string, number ou boolean.
+
+nome = prompt("Digite seu Nome: ")
+idade = prompt("Digite sua idade: ")
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+//eu usei o let acima para atribuir o nome da variável, mais aqui em baixa ela foi mudada sem precisar usar o let.
+
+console.log("Olá", nome, "você tem", idade, "anos",".Onde", nome,"e", idade, "são os valores que o usuári   o inseriu")
+

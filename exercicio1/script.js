2503f97bb042c33499bcdd72df060326325e9205
Joanadayse <daysejoana3@gmail.com>
2022-07-19 18:16:39 -0300

    Exercicios1-2-3

---
+++ exercicio1/script.js
@@ -0,0 +1,51 @@
+/const sorvete = prompt("Você gosta de sorvete?")
+const oculos = prompt("Você usa óculos?")
+const anime = prompt("Você gosta de animes?")
+
+console.log(sorvete)
+console.log(anime)
+console.log(oculos)/
+
+const sorveteperg = "Você gosta de sorvete?"
+const sorveteresp = prompt(sorveteperg)
+console.log(sorveteperg)
+console.log(sorveteresp)
+
+const oculosperg = "Você usa óculos?"
+const oculosresp =prompt(oculosperg)
+console.log(oculosperg)
+console.log(oculosresp)
+
+const animeperg = "Você gosta de animes?"
+const animeresp = prompt(animeperg)
+console.log(animeperg)
+console.log(animeresp)
+
+let a = 10;
+let b = 25;
+let c;
+
+//usamos uma variável "vazia" para conseguir alterar o valor das demais variáveis/
+c=a 
+a=b
+b=c
+
+console.log ("O novo valor de a é", a)
+console.log ("O novo valor de b é", b)
+/let nome = ""
+let idade =/
+
+//A resposta apareceu como "erro" porque não possui valores atribuídos/
+
+let nome = "Sebastiana"
+let idade = 35
+console.log (nome)
+console.log (idade)
+console.log (typeof nome)
+console.log (typeof idade)
+
+/*A resposta apareceu como "String" porque apesar de se referirem a valores (escrita e número) 
+diferentes, ambas são lidas como texto*/
+
+console.log ("Olá,", nome, "você tem", idade, "anos")
+

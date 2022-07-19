fe326eedc35dd34bfcdb1cf8ca37db26a3d4e78d
Ruan <ruanemidio1412@gmail.com>
2022-07-19 11:13:10 -0300

    Esse foi um exercicio de complementação do que desafiado em aula

---
+++ exercicio3/script.js
@@ -0,0 +1,15 @@
+// acrescentando valores as variaveis.
+A = 10
+B = 25
+// Deixando uma variavel vazia para circular e em seguida mudar os valores.
+C = 0
+
+// Invertendo os valores com uma variavel vazia de base para troca.
+C = A
+A = B
+B = C 
+
+// Imprimindo os valores no console do navegador.
+console.log("o novo valor de A é", A)
+console.log("o novo valor de B é", B)
+console.log("o novo valor de C é", A)

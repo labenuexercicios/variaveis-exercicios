ce5a17a48ec98ae93c00192c3cbcff5604b1c4de
Edson <edson.exe@outlook.com>
2022-07-19 20:33:20 -0300

    Exercício 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -2,5 +2,12 @@ let nome = window.prompt('Qual seu nome?');
 let idade = window.prompt('Quantos anos você tem?');
 let anoNasc = window.prompt('Qual seu ano de nascimento');
 
-window.alert(`Nome: ${nome}, Idade: ${idade}, Ano nascimento: ${anoNasc}`)
+// window.alert(`Nome: ${nome}, Idade: ${idade}, Ano nascimento: ${anoNasc}`)
 
+let resp1 = nome;
+let resp2 = idade;
+let resp3 = anoNasc;
+
+console.log(`${nome}: ${resp1}
+${idade}: ${resp2}
+${anoNasc}: ${resp3}`)

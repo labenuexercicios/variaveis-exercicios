39441e61c26aad6f98ddc14eb8a97ec5ea8e2463
Tayanne <tayh.snovaes@gmail.com>
2022-07-19 20:49:28 -0300

    atualiza o exercicio3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,15 +1,14 @@
-let A = 10;
-let B = 25;
-let C;
+// let A = 10;
+// let B = 25;
+// let C;
 
-C = A;
-A = B;
-B = C;
+// C = A;
+// A = B;
+// B = C;
 
-console.log('Valor de A', A);
-console.log('Valor de B', B);
+// console.log('Valor de A', A);
+// console.log('Valor de B', B);
 
-/* 
 let A = 20
 let B = 30
 
@@ -17,5 +16,4 @@ A = A + B // A=50; B=30
 B = A - B // A=50; B=20
 A = A - B // A=30; B=20
 
-   @ SOLUÇÃO BY DARVAS
-*/
+// @ SOLUÇÃO BY DARVAS

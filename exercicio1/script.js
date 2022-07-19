6fdba16a68ec1aa7f127483e500ba2a81ddd2153
RonalldoTeixeira <ronalldo.tb@gmail.com>
2022-07-19 19:01:31 -0300

    exercicio-07

---
+++ exercicio1/script.js
@@ -0,0 +1,17 @@
+let nome = ""
+let idade = 
+console.log  (nome)
+console.log  (idade)
+
+console.log  (typeof nome)
+console.log  (typeof idade)
+
+nome = "Kricia"
+ idade = 25
+console.log  (nome)
+console.log  (idade)
+
+console.log  (typeof nome)
+console.log  (typeof idade)
+
+ 
---
+++ exercicio2/script.js
@@ -0,0 +1,14 @@
+const pergunta1 = "você é Dj?"
+let Não = prompt (pergunta1)
+const pergunta2 = "Qual sua Profissão?"
+let eletronico = prompt (pergunta2)
+const pergunta3 = "Qual a reigão do Brasil você mora?"
+let nordeste = prompt(pergunta3)
+
+let Não = Dj
+let eletronico = profissão
+let nordeste = região
+
+console.log (pergunta1)
+console.log (pergunta2)
+console.log (pergunta3)

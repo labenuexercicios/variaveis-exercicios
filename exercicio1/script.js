9f40f34a08b753b79f5402af3bc76e06b3fc34d0
Adriano Silva <adrianowic0101@gmail.com>
2022-07-19 16:10:08 -0300

    Alterações feitas nos arquivos js das tres pastas.

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+let nome = prompt("Digite seu nome") 
+const idade = Number(prompt("Digite sua idade"))
+
+//Aqui foi impresso uma string, porque tudo que vem digitado do prompt, é considerado uma string.
+console.log(typeof nome)
+
+//Aqui também deveria ser uma string, já que, como foi dito antes, tudo que vem do prompt é considerado uma string. Porém, uma conversão de tipo primitikvo foi feita, transformando uma string em number, já que se tratando de idade, faz muito mais sentido ser um number.
+console.log(typeof idade)
+
+console.log("Ola" ,nome,",", "você tem" ,idade, "anos de idade.")
---
+++ exercicio2/script.js
@@ -0,0 +1,17 @@
+let per1 = "Já tomou vitamina de abacate ?"
+let res1 = prompt(per1)
+//-------------------------------------------------------
+
+let per2 = "Já bebeu água hoje ?"
+let res2 = prompt(per2)
+//-------------------------------------------------------
+
+let per3 = "Senhor dos aneis é melhor que harry potter ?"
+let res3 = prompt(per3)
+
+//-------------------------------------------------------
+
+
+console.log(per1,res1)
+console.log(per2,res2)
+console.log(per3,res3)

0f9023c65204b696b6026d9c14f51819cdebb451
Jessé Amaral <jesseelite286@gmail.com>
2022-07-19 19:43:59 -0300

    Exercicio-a007-feito

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+const nome = prompt("Qual seu nome?");
+
+let idade = Number(prompt("Qual sua idade?"));
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+console.log("Olá " + nome + " você tem " + idade + "  anos")

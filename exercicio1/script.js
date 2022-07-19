f59c1edb8ba5e53b226e315165edcec4f426c09d
Isabelly Martins <isabellyksml@gmail.com>
2022-07-18 23:26:27 -0300

    Concluído exercício a007

---
+++ exercicio1/script.js
@@ -0,0 +1,19 @@
+let nome = ""
+console.log(typeof nome)
+
+let idade;
+console.log(typeof idade)
+
+// Confirmando o tipo da variável, como a v. nome esta sem valor assim como a idade ()undefined, aí aparece string (vazia) 
+
+nome = (prompt("Qual é o seu nome"))
+idade = (prompt("Qual é a sua idade"))
+
+// Como a v. idade esta sem valor assim como a v. nome, aí aparece string para ambas
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+console.log("Olá!",nome, "você tem",idade, "anos")
+
+
---
+++ exercicio2/script.js
@@ -0,0 +1,19 @@
+// const perguntaA = prompt("Hoje vai chover?")
+// const perguntaB = prompt("Vai assistir aos jogos da Copa?")
+// const perguntaC = prompt("Você é de maior?")
+
+const perguntaA = ("Hoje vai chover?")
+const perguntaB = ("Vai assistir aos jogos da Copa?")
+const perguntaC = ("Você é de maior?")
+
+const respostaA = prompt(perguntaA)
+const respostaB = prompt(perguntaB)
+const respostaC = prompt(perguntaC)
+
+console.log(respostaA)
+console.log(respostaB)
+console.log(respostaC)
+
+console.log(perguntaA, respostaA)
+console.log(perguntaB, respostaB)
+console.log(perguntaC, respostaC)

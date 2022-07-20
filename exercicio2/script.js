ed8fe8cc0f0dc2626fe8a34c44c8c946faf5edef
Renan Lopes <r.maruffa@gmail.com>
2022-07-19 21:24:57 -0300

    Exercicio A007 Resolvido

---
+++ exercicio2/script.js
@@ -0,0 +1,19 @@
+// let carro=prompt("Voce tem carro?");
+// let funcionamento=prompt("Ele funciona?");
+// let pecas=prompt("E as peças pra arrumar? Ja chegaram?");
+
+// console.log(carro);
+// console.log(funcionamento);
+// console.log(pecas);
+
+const perguntaA = "Voce tem carro?";
+const perguntaB = "Ele funciona?";
+const perguntaC = "E as peças pra arrumar? Ja chegaram?";
+
+const respostaA = prompt(perguntaA);
+const respostaB = prompt(perguntaB);
+const respostaC = prompt(perguntaC);
+
+console.log(perguntaA, "-", respostaA);
+console.log(perguntaB, "-", respostaB);
+console.log(perguntaC, "-", respostaC);

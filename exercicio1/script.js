ccd5f817e6eabb685cecf7fe8f6e4df2ee164eb3
Joanadayse <daysejoana3@gmail.com>
2022-07-19 22:11:57 -0300

    exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 25;
+let c; //usamos uma variavel vazia para conseguir alterar o valor das demais variaveis 
+
+a=b
+b=c
+
+console.log ("o novo resultado de a é" a)
+console.log ("o no resultado de b é" b) // a resposta aparece como erro porque não possui valores atribuidos 

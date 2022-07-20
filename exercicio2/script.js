7e6668d624e80906da92582bdec7ea9f8aceb68d
RonalldoTeixeira <ronalldo.tb@gmail.com>
2022-07-19 21:35:22 -0300

    respostas do exercicio

---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+const per1 = "vc gosta de futebol?"
+const per2 = "vc gosta de musica?"
+const per3 = "vc gosta de galinha?"
+
+const resp1 = prompt (per1)
+const resp2 = prompt (per2)
+const resp3 = prompt(per3)
+
+console.log(per1, resp1)
+console.log(per2, resp2)
+console.log(per3, resp3)

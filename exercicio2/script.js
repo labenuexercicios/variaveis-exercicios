8f9e62116826848d7d61554fdb052648cf72fbfa
MilenaCruzJusta <milenajusta86@gmail.com>
2022-07-20 02:54:36 -0300

    subindoexercicio2

---
+++ exercicio2/script.js
@@ -0,0 +1,23 @@
+const resposta1 = "sim"
+const resposta2 = "sim"
+const resposta3 = "não"
+
+console.log(resposta1)
+console.log(resposta2)
+console.log(resposta3)
+
+//mandando a pergunta como variável
+const perguntaA = "Você acordou melhor hoje?";
+const respostaA = prompt(perguntaA);
+
+//mandando a pergunta como variável
+const perguntaB = "Você ainda sente dor em alguma parte do corpo?";
+const respostaB = prompt(perguntaB);
+
+//mandando a pergunta como variável
+const perguntaC = "Gostaria de receber visitas hoje?";
+const respostaC = prompt(perguntaC);
+
+console.log(perguntaA, respostaA)
+console.log(perguntaB, respostaB)
+console.log(perguntaC, respostaC)

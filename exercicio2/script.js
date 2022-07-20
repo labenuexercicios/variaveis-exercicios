c7a186a07a9bca9c3f394281e402a50ef4857336
Joanadayse <daysejoana3@gmail.com>
2022-07-19 22:13:08 -0300

    exercicio2

---
+++ exercicio2/script.js
@@ -0,0 +1,25 @@
+prompt ("como voce esta?")
+prompt ("dormiu bem?")
+prompt ("ja comeu?")
+
+// resposta letra a 
+let respostaA = "sim"
+let respostaB = "sim"
+let respostaC = "nao"
+
+//resposta letra b 
+console.log( respostaA, respostaB , respostaC)
+
+// resposta letra C 
+let perguntaA=prompt ("como voce esta?")
+let perguntaB=prompt ("dormiu bem?")
+let perguntaC=prompt ("ja comeu?")
+
+respostaA=prompt(perguntaA)
+respostaB=prompt(perguntaB)
+respostaC=prompt(perguntaC)
+
+// resposta letra D.
+console.log(perguntaA , respostaA)
+console.log(perguntaB , respostaB)
+console.log(perguntaC , respostaC)

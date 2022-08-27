7defdb621bcc55248adcb5c2b021e380eb147376
guirozmesquita@gmail.com <guilhermer_mesquita@hotmail.com>
2022-08-26 22:29:11 -0400

    exercicios js

---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+let maca = "Você gosta de maçã?"
+let viagem = "Você já viagem de avião?"
+let musica = 'VocÊ sabe tocar algum instrumento?'
+
+let s1 = prompt(maca)
+let n = prompt(viagem)
+let s2 = prompt(musica)
+
+alert(maca + "-" + s1)
+alert(viagem + "-" + n)
+alert(musica + "-" + s2)
---
+++ exercicio3/script.js
@@ -0,0 +1,10 @@
+let a = 10
+let b = 25
+
+let coringa = a
+
+a = b
+b = coringa
+
+console.log("O novo valor de a é", a) 
+console.log("O novo valor de b é", b)

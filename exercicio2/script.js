70eb487c23fdea8ba4501b8d1578884d99a00d08
veronicarubim <veronica.rubim97@gmail.com>
2022-08-29 16:05:55 -0300

    Todos

---
+++ exercicio2/script.js
@@ -0,0 +1,25 @@
+//const Resposta1 = prompt("Você gosta de macarrão?")
+//const Resposta2 = prompt("Você usa tênis?")
+//const Resposta3 = prompt ("Gatos dormem pouco?")
+
+
+
+//const Pergunta1 = prompt("Você gosta de macarrão?"); 
+//const Resposta1 = prompt(Pergunta1)
+
+//const Pergunta2 = prompt("Você usa tênis?");
+//const Resposta2 = prompt(Pergunta2)
+
+//const Pergunta3 = prompt ("Gatos dormem pouco?");
+//const Resposta3 = prompt(Pergunta3)
+
+
+
+const Pergunta1 = prompt("Você gosta de macarrão?"); 
+const Resposta1 = prompt(`Você gosta de macarrão? ${Pergunta1}`)
+
+const Pergunta2 = prompt("Você usa tênis?");
+const Resposta2 = prompt(`Você usa tênis? ${Pergunta2}`)
+
+const Pergunta3 = prompt ("Gatos dormem pouco?");
+const Resposta3 = prompt(`Gatos dormem pouco? ${Pergunta3}`)

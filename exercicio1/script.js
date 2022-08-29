689c6ef0694c1580128aadfe5154c6bde57d0b48
CaioCmara <caio1801@gmail.com>
2022-08-29 15:56:05 -0300

    commit definitivo

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,11 +1,20 @@
-const nome = prompt("Qual é o seu nome?")
-console.log (nome)
+const nome = null
+let idade = null
 
-let idade = prompt ("Qual é a sua idade?")
-idade = Number(idade)
-console.log (idade)
- 
-console.log("Olá", nome, " você tem ", idade, "anos")
-
+//tipos das variáveis - as duas estão sendo lidas apenas como "object"
 console.log(typeof(nome))
 console.log(typeof(idade))
+
+//A variável não estava funcionando sem valor atribuído
+
+const nomePrompt = prompt("Qual é o seu nome?")
+console.log (nomePrompt)
+
+let idadePrompt = prompt ("Qual é a sua idade?")
+idadePrompt = Number(idadePrompt)
+console.log (idadePrompt)
+ 
+console.log("Olá", nomePrompt, " você tem ", idadePrompt, "anos")
+// o tipo das variaveis 
+console.log(typeof(nomePrompt))
+console.log(typeof(idadePrompt))

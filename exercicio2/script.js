6cd851d515a0529e0350c8b6e3191e8105762f99
gabrielsan179 <gabrielsan179@gmail.com>
2022-08-29 14:51:14 -0300

    realizei os exercicios

---
+++ exercicio2/script.js
@@ -0,0 +1,20 @@
+//Escreva um programa que faça 3 perguntas de Sim ou Não, e guarde cada resposta em uma variável. Por exemplo: "Você está usando uma roupa azul hoje?". Depois, siga os passos:
+let gostaDeFilme = prompt("você gosta de filme?")
+let gostaDeMusica = prompt("você gosta de musica?")
+let gostaDeEsportes = prompt("você gosta de esporte?")
+//Crie três novas variáveis, contendo as respostas;
+//Imprima na tela todas as respostas
+console.log(gostaDeFilme)
+console.log(gostaDeMusica)
+console.log(gostaDeEsportes)
+//Altere seu programa, para que o texto das perguntas também sejam guardadas em variáveis. Seu comando de **prompt** deve mudar um pouco,
+const perguntaFilme = "você gosta de filme?"
+const respostaFilme = prompt(perguntaFilme)
+const perguntaMusica = "você gosta de musica?"
+const respostaMusica = prompt(perguntaMusica)
+const perguntaEsporte = "você gosta de esporte?"
+const respostaEsporte = prompt(perguntaEsporte)
+//Altere seu programa, para que a impressão das respostas também tenha as **perguntas**.
+console.log(perguntaFilme, respostaFilme)
+console.log(perguntaMusica, respostaMusica)
+console.log(perguntaEsporte, respostaEsporte)

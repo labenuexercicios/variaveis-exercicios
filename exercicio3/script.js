f46a8f9789df56f23042e6447caa4100c0874319
marcoshamil76 <marcoshamilton1976@gmail.com>
2022-08-29 16:39:13 -0300

    Entrega exercicio 03

---
+++ exercicio3/script.js
@@ -0,0 +1,18 @@
+// let prioridade1 = "Oração"
+// let prioridade2 = "Família"
+
+
+// console.log (`Minhas principais prioridades são 1° ${prioridade1} e em 2° lugar ${prioridade2}`)
+
+// codigo acima cumpre primeira parte do Exercicio, abaixo altero as variaveis de prioridades
+
+
+let prioridade1 = "Oração"
+let prioridade2 = "Família"
+let auxiliar = prioridade1
+console.log (`Prioridade 1= ${prioridade1} Prioridade 2=${prioridade2}`)
+// agora altero a ordem das prioridades
+prioridade1 = prioridade2
+prioridade2 = auxiliar
+
+console.log (`Minhas principais prioridades são 1° ${prioridade1} e em 2° lugar ${prioridade2}`)

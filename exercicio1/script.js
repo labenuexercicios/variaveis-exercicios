b510bbdbe37f3dd00524e8ff6410ae4f0bc329fd
veronicarubim <veronica.rubim97@gmail.com>
2022-08-29 14:48:40 -0300

    Exercicio2

---
+++ exercicio1/script.js
@@ -0,0 +1,15 @@
+//let nome
+//let idade
+//console.log (nome, typeof nome)
+//console.log (idade, typeof idade)
+
+// As duas variáveis são em formato de texto, ou strings, e não expressam valores numéricos.
+// Para tal, eu deveria executar um código para transformá-las em números. 
+
+let nome = prompt ("Diga seu nome")
+let idade = prompt ("Diga sua idade")
+console.log (idade, typeof idade)
+console.log (nome, typeof nome)
+
+console.log (`Olá ${nome}, você tem ${idade} anos`)
+

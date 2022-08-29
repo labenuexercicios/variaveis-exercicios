85b08c94c94b9ef2b9f7fada5904d6f7126e7b7b
laisrmacedo <laisrodriguesmacedo@gmail.com>
2022-08-29 18:34:21 +0200

    exer1: e, f e g

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,7 +1,18 @@
 //a, b, c, d
 
-let nome 
-let idade 
-console.log(typeof nome, typeof idade)
+// let nome 
+// let idade 
+// console.log(typeof nome, typeof idade)
 
 //Foi impresso undefined pq não foi atribuido valor as variaveis.
+
+//e , f, g
+
+const nome = prompt("Qual seu nome?");
+let idade = prompt("Quantos anos vc tem?");
+
+console.log(typeof nome, typeof idade)
+//Mesmo sendo um numero, a variavel idade 
+//tem o tipo string pq o prompt sempre retorna este tipo.
+
+console.log(`Olá ${nome},  você tem ${idade} anos". Onde ${nome} e ${idade} são os valores que o usuário inseriu`)

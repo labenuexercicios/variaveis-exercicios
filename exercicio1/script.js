00b3fa5c3214720da6961b920f3b6859febfa984
marcoshamil76 <marcoshamilton1976@gmail.com>
2022-08-29 14:47:26 -0300

    exercicio01 ok

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+let nome  = prompt("Qual o seu nome?")
+let idade = prompt("Qual a sua idade?")
+console.log(typeof nome)
+console.log(typeof idade)
+
+console.log(`Eita ${nome}, ${idade} já? Tá ficando velho hein!!!`)

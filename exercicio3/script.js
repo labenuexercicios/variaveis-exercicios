d6e920b08b6e827de6a26c720153136dcbee69ff
Mjfreedom <mbuzina@gmail.com>
2022-08-29 16:07:09 -0300

    Exercicio 3 Finalizado

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -4,5 +4,10 @@ let b = 120
 //console.log(a, b)
 console.log(`A ${a}, B ${b}`)
 
-console.log("O novo valor de a é", b)
-console.log("O novo valor de b é", a)
+let c = a
+a = b
+b = c
+
+//console.log("A", a, "B", b)
+console.log("O novo valor de a é", a)
+console.log("O novo valor de b é", b)

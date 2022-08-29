0b4d0e55976dda0312fd0122fd090237b940dae9
euleo11 <leonardov.silva15@gmail.com>
2022-08-29 15:58:52 -0300

    Finalizei exercicio 2 e refiz o exercicio 1

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,6 +1,6 @@
 let nome = prompt("Qual é o seu nome ?")
 console.log(nome, typeof nome) // Muito complicado, mas eu vou conseguir!
-let idade = prompt("Quantos anos você tem ?")
+let idade = Number(prompt("Quantos anos você tem ?"))
 console.log(idade, typeof idade,)
 console.log(`Olá ${nome}, você tem ${idade} anos`)
 
---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+const bebeuAgua = prompt("Bebeu água hoje ?")
+// const seBebeuAgua = (bebeuAgua)
+console.log(`Bebeu água ? - ${bebeuAgua}`)
+
+const respiracao = prompt("Ta respirando ?")
+// const seRespirou = (respiracao)
+console.log(`Está vivo ? - ${respiracao}`)
+
+const lolzin = prompt("Já perdeu x1 pra mim no Lol?")
+// const sePerdeu = (lolzin)
+console.log(`Você já perdeu no x1 pra mim ? - ${lolzin}`)

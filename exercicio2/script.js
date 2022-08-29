b2b35ca08ad6f2c810caaa7021cfd8cbdd0c2030
pkamibayashi <paulo_Kamibayashi@hotmail.com>
2022-08-29 13:43:30 -0300

    Feito alteração

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -5,23 +5,27 @@ let respostaCasa = prompt(casa)
 const carro = 'Você tem carro?'
 let respostaCarro = prompt(carro)
 
-console.log (dinheiro, casa, carro)
-console.log(`Você tem dinheiro? - ${respostaCarro}, Você tem casa? - ${respostaCasa}, Você tem carro? - ${respostaDinheiro}`)
 
-if (respostaDinheiro === 'sim'){
-  console.log('Você tem dinheiro')
-} else {
-  console.log('Você não tem dinheioro')
-};
 
-if (respostaCasa === 'sim'){
-  console.log('Você tem uma casa')
-} else {
-  console.log('Você não tem casa')
-}
 
-if (respostaCarro === 'sim'){
-  console.log('Você tem carro')
-} else{
-  console.log('Você não tem carro')
-}
+
+// console.log (respostaDinheiro, respostaCasa, respostaCarro)
+console.log(`Você tem dinheiro? - ${respostaDinheiro}, Você tem casa? - ${respostaCasa}, Você tem carro? - ${respostaCarro}`) 
+
+// if (respostaDinheiro === 'sim'){
+//   console.log('Você tem dinheiro')
+// } else {
+//   console.log('Você não tem dinheioro')
+// };
+
+// if (respostaCasa === 'sim'){
+//   console.log('Você tem uma casa')
+// } else {
+//   console.log('Você não tem casa')
+// }
+
+// if (respostaCarro === 'sim'){
+//   console.log('Você tem carro')
+// } else{
+//   console.log('Você não tem carro')
+// }

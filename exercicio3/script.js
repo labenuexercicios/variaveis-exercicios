88d30500f7295b7fbd408b956f8619510b5dd90a
mbiga <mbigaram.mb@gmail.com>
2022-08-29 17:04:47 -0300

    Exercicios 1,2 e 3

---
+++ exercicio3/script.js
@@ -0,0 +1,12 @@
+let a = 10
+let c = a
+let b = 25
+
+console.log("O valor de a é", a)
+console.log("O valor de b é", b)
+
+a = b
+b = c
+
+console.log("O novo valor de a é", a)
+console.log("O novo valor de b é", b)

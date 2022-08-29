23475c85c7871a4cc0dbfc079441c33460604257
jessicayve <yve.jessica@gmail.com>
2022-08-29 15:11:40 -0300

    exercicio 2 resolvido

---
+++ exercicio2/script.js
@@ -0,0 +1,14 @@
+const perguntaA = "Você é mulher?"
+const respostaA = prompt(perguntaA)
+
+const perguntaB = "Você é homem?"
+const respostaB = prompt(perguntaB)
+
+const perguntaC = "Você tem tatuagem?"
+const respostaC = prompt(perguntaC)
+
+console.log(perguntaA, respostaA)
+console.log(perguntaB, respostaB)
+console.log(perguntaC, respostaC)
+
+

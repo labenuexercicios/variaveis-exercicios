fce60e699994bebf199f1df2d7743b42ad7d6952
laisrmacedo <laisrodriguesmacedo@gmail.com>
2022-08-29 18:39:15 +0200

    exer2: c

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,23 @@
 //a, b
 
-let pergunta1 = prompt("Voce acordou antes das 8h hoje? SIM ou NÂO")
-let pergunta2 = prompt("Voce ja bebeu agua hoje? SIM ou NÂO")
-let pergunta3 = prompt("Voce entendeu 100% da aula de hoje? SIM ou NÂO")
+// let pergunta1 = prompt("Voce acordou antes das 8h hoje? SIM ou NÂO")
+// let pergunta2 = prompt("Voce ja bebeu agua hoje? SIM ou NÂO")
+// let pergunta3 = prompt("Voce entendeu 100% da aula de hoje? SIM ou NÂO")
 
-let resposta1 = console.log(pergunta1)
-let resposta2 = console.log(pergunta2)
-let resposta3 = console.log(pergunta3)
+// let resposta1 = console.log(pergunta1)
+// let resposta2 = console.log(pergunta2)
+// let resposta3 = console.log(pergunta3)
+
+//c
+
+let pergunta1 = "Voce acordou antes das 8h hoje? SIM ou NÂO"
+let resposta1 = prompt(pergunta1)
+console.log(resposta1)
+
+let pergunta2 = "Voce ja bebeu agua hoje? SIM ou NÂO")
+let resposta2 = prompt(pergunta2)
+console.log(resposta2)
+
+let pergunta3 = "Voce entendeu 100% da aula de hoje? SIM ou NÂO")
+let resposta3 = prompt(pergunta3)
+console.log(resposta3)

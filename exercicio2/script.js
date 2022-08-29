04382081b5e3f0d22456d9e1db724a3b2974f74a
slvgs <gabriellasilveriosantana15@gmail.com>
2022-08-29 15:58:37 -0300

    Exercicios Completos

---
+++ exercicio2/script.js
@@ -0,0 +1,14 @@
+
+//Prompt sempre será String
+
+const perguntaA = "Você está usando uma roupa azul hoje?"
+const respostaA = prompt(perguntaA)
+console.log("Você esta usando azul hoje?", respostaA)
+
+const perguntaB = "Você está usando uma roupa verde hoje?"
+const respostaB = prompt(perguntaB)
+console.log("Você esta usando verde hoje?", respostaB)
+
+const perguntaC = "Você está usando uma roupa lilás hoje?"
+const respostaC = prompt( perguntaC)
+console.log("Você esta usando lilás hoje?", respostaC)

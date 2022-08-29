6221d299a486e2754fe8527931a76930f6fdd4f3
jessicayve <yve.jessica@gmail.com>
2022-08-29 14:19:47 -0300

    exercicio resolvido

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+let nome = prompt("Qual é o seu nome?")
+let idade = prompt("Qual é a sua idade?")
+console.log(nome, typeof nome)
+console.log(idade, typeof idade)
+// porque não se deu valor as variaveis

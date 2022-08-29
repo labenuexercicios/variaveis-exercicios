4972e8f15fa7986e5597ad8740f3328737e2073f
guirozmesquita@gmail.com <guilhermer_mesquita@hotmail.com>
2022-08-29 11:51:50 -0400

    exercicios

---
+++ exercicio1/script.js
@@ -0,0 +1,14 @@
+// let nome = 
+// let idade =
+
+// console.log(typeof(nome))
+// console.log(typeof(idade))
+
+//Dará erro pois as variáveis não têm valor
+
+let nome = 'Guilherme';
+let idade = 18
+
+console.log(typeof(nome),typeof(idade))
+//Agora irá dar certo, pois as váriaveis têm seus valores
+console.log(`Olá ${nome}, você tem ${idade} anos`)
---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+let likeApple = "Você gosta de maçã?"
+let hasTrip = "Você já viagem de avião?"
+let playMusic = 'VocÊ sabe tocar algum instrumento?'
+
+let s1 = prompt(likeApple)
+let n = prompt(hasTrip)
+let s2 = prompt(playMusic)
+
+alert(likeApple + "-" + s1)
+alert(hasTrip + "-" + n)
+alert(playMusic + "-" + s2)

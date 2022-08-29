e3ce7707bbd570ca82790068e5642b6adecf328f
unknown <iagosto.nasc@gmail.com>
2022-08-29 16:23:49 -0300

    commit A007

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+let nome = prompt("Qual seu nome?") 
+
+let idade = prompt("Qual sua idade?")
+
+//console.log(typeof nome);
+//.log(typeof idade); 
+
+console.log("olá", nome, "tudo bem? você tem", idade, "anos de idade");
---
+++ exercicio2/script.js
@@ -0,0 +1,12 @@
+const Pergunta1 = ("já se sentiu ameaçado por um prompt de computador?")
+let Resposta1 = prompt(Pergunta1)
+
+const Pergunta2 = ("Você tem certeza disso?")
+let Resposta2 = prompt(Pergunta2)
+
+const Pergunta3 = ("ESSA É SUA RESPOSTA FINAL?")
+let Resposta3 = prompt(Pergunta3)
+
+console.log(Pergunta1,"R=", Resposta1);
+console.log(Pergunta2,"R=", Resposta2);
+console.log(Pergunta3,"R=", Resposta3);

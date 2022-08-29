526aef7c234472efe69392be726ac9a8fa7ebd15
heyalearaujo <alexandrearza@gmail.com>
2022-08-29 14:26:17 -0300

    Resolucao exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+let nome = prompt ("Qual seu nome?")
+console.log(nome, typeof nome)
+
+let idade = Number (prompt ("Qual sua idade?"))
+console.log(idade, typeof idade)
+
+console.log(`Olá, ${nome}, você tem ${idade} anos.`)
+
+// Novamente, imprima na tela o tipo dessas variáveis. 
+// O que você notou? Escreva em um comentário de código.
+// Após o usuario colocar as informações pedida pelo codigo, 
+// as variaveis foram adicionadas ao console com seu tipo ao lado.

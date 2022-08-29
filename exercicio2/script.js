420f9c979f3b77c43004563fea6568717fef49db
lucasrodriguesg <lucas.rodrigues.pvd@gmail.com>
2022-08-29 15:07:40 -0300

    Exercicios resolvidos

---
+++ exercicio2/script.js
@@ -0,0 +1,18 @@
+
+/*const Pergunta1 = prompt ('Você gosta de doce? (Sim ou Não)')
+const Pergunta2 = prompt ('Você gosta de presentes? (Sim ou Não)')
+const Pergunta3 = prompt ('Você namora? (Sim ou Não)')
+
+console.log(Pergunta1, Pergunta2, Pergunta3)*/
+
+
+const pergunta1 = ('Você gosta de doce? (Sim ou Não)')
+const Pergunta1 = (prompt(pergunta1))
+const pergunta2 = ('Você gosta de presentes? (Sim ou Não)')
+const Pergunta2 = (prompt(pergunta2))
+const pergunta3 = ('Você namora? (Sim ou Não)')
+const Pergunta3 = (prompt(pergunta3))
+
+console.log(pergunta1, Pergunta1)
+console.log(pergunta2, Pergunta2)
+console.log(pergunta3, Pergunta3)

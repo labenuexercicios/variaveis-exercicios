9e90eddbe54de6a5fd1d304393ee56380245f5da
LuizHenriqueMarinho <luizhenriquemarinhofga@gmail.com>
2022-08-29 14:46:37 -0300

    a007

---
+++ exercicio3/script.js
@@ -0,0 +1,14 @@
+/*let a = 10
+let b = 25
+a=b
+b=10
+
+console.log("o novo valor de a é: ", a)
+console.log("o novo valor de b é: ", b)*/
+
+let a
+let b
+console.log("digite o valor de a", b= prompt("digite o valor de a")) //inverte o a e o b
+console.log("digite o valor de b", a= prompt("digite o valor de b"))
+console.log("o valor de b foi trocado e é:", b)
+console.log("o valor de a foi trocado e é:", a)

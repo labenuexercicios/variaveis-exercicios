0daafb217c6090c7dc4476674860ac9a770a6527
PYTERODACTYL <pyter512@live.com>
2022-08-29 16:11:34 -0300

    feito

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+let nome1 = undefined;
+let idade1 = undefined; 
+console.log(typeof nome1);
+console.log(typeof idade1);
+nome1 = prompt ("Qual o seu nome?");
+idade1 = prompt ("Qual a sua idade?");
+console.log(typeof nome1);
+console.log(typeof idade1);
+console.log ("Olá "+nome1+" sua idade é "+idade1);
---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+let pergunta1 = "Você almoçou hoje?"
+let pergunta2 = "Você tomou banho hoje?"
+let pergunta3 = "Você bebeu água hoje?"
+
+let resposta1 = prompt(pergunta1)
+let resposta2 = prompt(pergunta2)
+let resposta3 = prompt(pergunta3)
+
+console.log (pergunta1,resposta1)
+console.log (pergunta2,resposta2)
+console.log (pergunta3,resposta3)

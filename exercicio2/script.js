1070fb9aa5cf2600446ae1b9d943664c52a5f59c
pkamibayashi <paulo_Kamibayashi@hotmail.com>
2022-08-29 13:18:45 -0300

    Excercicio2 done

---
+++ exercicio2/script.js
@@ -0,0 +1,27 @@
+const dinheiro = 'Você tem dinheiro?'
+let respostaDinheiro = prompt(dinheiro)
+const casa = 'Você tem casa?'
+let respostaCasa = prompt(casa)
+const carro = 'Você tem carro?'
+let respostaCarro = prompt(carro)
+
+console.log (dinheiro, casa, carro)
+console.log(`Você tem dinheiro? - ${respostaCarro}, Você tem casa? - ${respostaCasa}, Você tem carro? - ${respostaDinheiro}`)
+
+if (respostaDinheiro === 'sim'){
+  console.log('Você tem dinheiro')
+} else {
+  console.log('Você não tem dinheioro')
+};
+
+if (respostaCasa === 'sim'){
+  console.log('Você tem uma casa')
+} else {
+  console.log('Você não tem casa')
+}
+
+if (respostaCarro === 'sim'){
+  console.log('Você tem carro')
+} else{
+  console.log('Você não tem carro')
+}

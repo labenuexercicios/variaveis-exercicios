0e8227b38c8046cb9520cfd4b84247c9ef74d317
mbiga <mbigaram.mb@gmail.com>
2022-08-29 14:56:31 -0300

    Exercicio 1 A007

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+// retornou string porque eu não pedi pra me passar alguma informação "prompt", apenas pra imprimir.
+const nome= prompt("Qual o seu nome?")
+let idade= prompt("Quantos anos você tem?")
+console.log("Olá "+nome+", você tem",idade,"anos")
+//console.log(nome, idade)
+//console.log(idade, typeof idade)

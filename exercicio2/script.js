1a9a74dba697068c9ea7d111937ef1146da1355f
gabrieljosephmaciel <gabrieljosephmaciel@gmail.com>
2022-08-29 18:01:54 -0300

    exercício 2 separado

---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+let agua = "Bebeu água?"
+let respostaagua = prompt(agua)
+let sede = "Tá com sede?"
+let respostasede = prompt(sede)
+let barbecue = "Would you like to eat barbecue today?"
+let respostabarbecue = prompt(barbecue)
+
+console.log(respostaagua, respostasede, respostabarbecue)
+console.log(agua, ":", respostaagua)
+console.log(sede, ":", respostasede)
+console.log(barbecue, ":", respostabarbecue)

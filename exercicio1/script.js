1dc09c88c340aff65d0deebac11e3dd59b23f3cc
VictorOgataj8 <victor.ogataj8@gmail.com>
2022-08-29 13:26:36 -0300

    exercicio 1 feito

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+// let nome
+// let idade 
+// console.log(typeof nome, typeof idade)
+//esse tipo de variavel foi criado para que em outro momento seje atribuido valor a essas variaveis.
+
+const Nome= prompt("qual e o seu nome?")
+let Idade= Number(prompt("qual e a sua idade?"))
+console.log(typeof Nome, typeof Idade)
+//eu nao tive erros nessa parte, notei que se eu nao atribuir valora a variavel idade ela continuaria como string, entao voce tem que atribuir manualmente.
+console.log("Ola meu nome e", Nome, "eu tenho tenho", Idade,"anos")

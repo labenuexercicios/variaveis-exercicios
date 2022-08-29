7d7e47571dd63700170e097d9690c1dd92c2332f
cesaralmeida002 <cesaralmeida002@gmail.com>
2022-08-29 15:58:35 -0300

    Exercicios 1-2-3 Completos

---
+++ exercicio2/script.js
@@ -0,0 +1,15 @@
+
+let pergunta1 = "Voce ja programa?"
+let resposta1 = prompt(pergunta1)
+
+let pergunta2 = "Trabalha como programador?"
+let resposta2 = prompt(pergunta2)
+
+let pergunta3 = "Quanto tempo?"
+let resposta3 = prompt(pergunta3)
+
+console.log(pergunta1, resposta1)
+
+console.log(pergunta2, resposta2)
+
+console.log(pergunta3, resposta3)

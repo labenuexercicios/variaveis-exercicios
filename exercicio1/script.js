b1a2e42f728293eb98a5d2b4d49ac86a6d35c7cc
CaioCmara <caio1801@gmail.com>
2022-08-29 13:12:25 -0300

    Exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+const nome = prompt("Qual é o seu nome?")
+console.log (nome)
+
+let idade = prompt ("Qual é a sua idade?")
+idade = Number(idade)
+console.log (idade)
+ 
+console.log("Olá", nome, " você tem ", idade, "anos")
+
+console.log(typeof(nome))
+console.log(typeof(idade))

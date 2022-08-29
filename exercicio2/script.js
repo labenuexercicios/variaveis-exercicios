c832bf03fd607dee2f8f95cec5e2753991f04a84
Mjfreedom <mbuzina@gmail.com>
2022-08-29 15:23:52 -0300

    Exercicio 2 Finalizado

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -9,5 +9,8 @@ const suspense = "Você gosta de Suspense?"
 const respostaSuspense = prompt(suspense)
 
 //b)
-console.log(serie, filmes, suspense)
-console.log(`Você assisti série? ${respostaSerie}, Você gosta de filmes? ${respostaFilmes}, Você gosta de Suspense? ${respostaSuspense}`)
+//console.log(serie, filmes, suspense)
+//console.log(`Você assisti série? ${respostaSerie}, Você gosta de filmes? ${respostaFilmes}, Você gosta de Suspense? ${respostaSuspense}`)
+console.log(serie, respostaSerie)
+console.log(filmes, respostaFilmes)
+console.log(suspense, respostaSuspense)

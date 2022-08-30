700be71496f2e5f853ab067b95f088464a200905
taisserodio <taisserodio12@gmail.com>
2022-08-30 00:38:25 -0300

    Primeiro Exercicio de html

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+let nome;
+
+let idade;
+
+nome = prompt("Qual seu nome?")
+idade = prompt("Qual sua idade?")
+
+console.log(typeof("Essa variavel é:", nome))
+console.log(typeof("Essa variavel é:", idade))
+
+console.log("Olá", nome, ("voce tem:"), idade)

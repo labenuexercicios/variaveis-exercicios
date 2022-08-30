9a85d04a916c529e284e3d950ba62572a2fca532
Rafaella Estecio <rafaellaestecio@gmail.com>
2022-08-29 21:04:15 -0300

    exercicio 2

---
+++ exercicio2/script.js
@@ -0,0 +1,21 @@
+/*const perguntaA = prompt("Você está usando uma roupa azul hoje?");
+const perguntaB = prompt("Você já bebeu agua?");
+const perguntaC = prompt("Você se exercitou?");*/
+
+/*let respostaA = perguntaA
+let respostaB = perguntaB
+let respostaC = perguntaC*/
+
+
+const perguntaA = "Você está usando uma roupa azul hoje?";
+const respostaA = prompt(perguntaA);
+
+const perguntaB = "Você já bebeu agua?";
+const respostaB = prompt(perguntaB);
+
+const perguntaC = "Você se exercitou?";
+const respostaC = prompt(perguntaC);
+
+console.log(perguntaA,respostaA)
+console.log(perguntaB,respostaB)
+console.log(perguntaC,respostaC)

9928c7e96e31cb908d1355e3124749c315219744
Mascariep <edipohc@gmail.com>
2022-08-29 21:19:55 -0300

    exercicio2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,7 +1,12 @@
-const resposta1 = prompt("O Palmeiras joga hoje?");
-const resposta2 = prompt("O Palmeiras é lider do campeonato?");
-const resposta3 = prompt("O Palmeiras vai ganhar amanha?");
+let pergunta1 = "O Palmeiras joga hoje?"
+let resposta1 = prompt(pergunta1);
 
-console.log(resposta1);
-console.log(resposta2);
-console.log(resposta3);
+let pergunta2 = "O Palmeiras é lider do campeonato?"
+let resposta2 = prompt(pergunta2);
+
+let pergunta3 = "O Palmeiras vai ganhar amanha?"
+let resposta3 = prompt(pergunta3);
+
+console.log("O Palmeiras joga hoje?", resposta1);
+console.log("O Palmeiras é lider do campeonato?", resposta2);
+console.log("O Palmeiras vai ganhar amanha?", resposta3);

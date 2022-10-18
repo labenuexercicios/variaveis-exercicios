3b1aa33fc0e00dd62f7234a5effd71d869a64338
LucasBreia <lucas.devfullstack021@gmail.com>
2022-10-18 00:14:19 -0300

    exercicio 3 em andamentoo

---
+++ exercicio3/script.js
@@ -0,0 +1,23 @@
+//EXERCICIO 3 
+
+const nomeCompleto = "Digite seu nome completo:";
+const respostanomeCompleto = prompt(nomeCompleto);
+
+const dataNascimento = "Digite sua data de nascimento:";
+const respostadataNascimento = prompt(dataNascimento);
+
+const endereco = "Digite o seu endereço:";
+const respostaendereco = prompt(endereco);
+
+const cpf = "Digite seu CPF:";
+const respostacpf = prompt(cpf);
+
+const escolaridade = "Qual é a sua escolaridade?";
+const respostaescolaridade = prompt(escolaridade);
+
+const cnh = "Possui CNH?";
+const respostacnh = prompt(cnh);
+
+
+
+

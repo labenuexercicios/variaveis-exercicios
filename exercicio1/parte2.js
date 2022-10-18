838300310081388863012f9585435f6d5d82e9b7
bartomsilva <bartomsilva@gmail.com>
2022-10-18 14:26:46 -0300

    feat: parte 2, texto das perguntas atribuído a variáves

--- exercicio1/parte2.js
+++ exercicio1/parte2.js
@@ -1,6 +1,9 @@
 
-const resposta1 = prompt("Você sabe a diferença entre const e let? (sim/não)")
-const resposta2 = prompt("Você gosta de suco de laranja? (sim/não)")
-const resposta3 = prompt("Você jé foi ao Monte Sinai? (sim/não)")
+const pergunta1 = "Você sabe a diferença entre const e let? (sim/não)"
+const pergunta2 = "Você gosta de suco de laranja? (sim/não)"
+const pergunta3 = "Você jé foi ao Monte Sinai? (sim/não)"
+const resposta1 = prompt(pergunta1)
+const resposta2 = prompt(pergunta2)
+const resposta3 = prompt(pergunta3)
 
 console.log(resposta1,"-",resposta2,"-",resposta3)

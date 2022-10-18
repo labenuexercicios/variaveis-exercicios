446a6220e8d763438852c56702bfae1e41c53a09
WeslleiBrito <wesllei.wbs@gmail.com>
2022-10-18 20:03:03 -0300

    Exercicio 1 completo

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -5,3 +5,27 @@ console.log("Tipo nome", typeof nome)
 console.log("Tipo idade", typeof idade)
 
 /* Foi impresso este resultado, pois não foi atribuido valor algum a variável */
+nome = prompt("Qual o seu nome?")
+idade = prompt("Informe a sua idade")
+
+console.log("Tipo nome", typeof nome)
+console.log("Tipo idade", typeof idade)
+
+/* Como foi atribuido valores as variaveis, fui mudado o tipo delas a partir da linha 8 */
+
+console.log("Olá", nome, "Você tem", idade, "anos")
+
+/* Parte 2 */
+
+const perguntaA = "Você é hetero?"
+const respostaA = prompt(perguntaA)
+
+const perguntaB = "Você gosta de esporte?"
+const respostaB = prompt(perguntaB)
+
+const perguntaC = "Você é programador profissional?"
+const respostaC = prompt(perguntaC)
+
+console.log(perguntaA, "-", respostaA)
+console.log(perguntaB, "-", respostaB)
+console.log(perguntaC, "-", respostaC)

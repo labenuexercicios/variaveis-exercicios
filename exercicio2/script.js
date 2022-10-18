cc223ab66923c9a8b9c6ff7d572e172c263eacbd
gomesvini <pain-nici2011@live.com>
2022-10-18 01:08:05 -0300

    resposta do exercicio 2

---
+++ exercicio2/script.js
@@ -0,0 +1,13 @@
+a = 10
+b = 25
+c = null
+
+console.log("O valor de A é", a)
+console.log("O valor de B é", b)
+
+c = a
+a = b
+b = c
+
+console.log("O novo valor de A agora é", a)
+console.log("O novo valor de B agora é", b)

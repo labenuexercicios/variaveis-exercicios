8846d53d50dc7f212109249bc2cd44ac57f1d3ea
bartomsilva <bartomsilva@gmail.com>
2022-10-18 14:30:54 -0300

    feat: parte 2, adicionado impressão das perguntas e respostas

--- exercicio1/parte2.js
+++ exercicio1/parte2.js
@@ -6,4 +6,6 @@ const resposta1 = prompt(pergunta1)
 const resposta2 = prompt(pergunta2)
 const resposta3 = prompt(pergunta3)
 
-console.log(resposta1,"-",resposta2,"-",resposta3)
+console.log(pergunta1, "-",resposta1)
+console.log(pergunta2, "-",resposta2)
+console.log(pergunta3, "-",resposta3)

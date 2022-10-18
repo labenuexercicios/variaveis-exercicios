864874cb151575f7e4b74c5c5e1acab2ad085b43
Renan <ren4n_oliveira@hotmail.com>
2022-10-18 20:15:07 -0300

    Atualizado Exercicio 3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -4,24 +4,25 @@ const data = prompt ("Qual sua data de nascimento?")
 
 const end = prompt ("Qual é o seu endereço?")
 
-let cpf = prompt ("Digite seu cpf")
+let cpf = Number(prompt ("Digite seu cpf"))
 
 const escolaridade = prompt ("Qual é a sua escolaridade?")
 
 const cnh = prompt ("Possui CNH?")
 
-let filhos = prompt ("Quantos filhos tem?")
+let filhos = Number(prompt ("Quantos filhos tem?"))
 
 const cargo = prompt ("Qual é o seu cargo atual?")
 
-let salario = prompt ("Qual é o seu salário atual?")
+let salario = Number (prompt ("Qual é o seu salário atual?"))
 
-let comissao = prompt ("Recebe comissão? se sim digite a porcentagem, se não digite 0")
+let comissao = Number (prompt ("Recebe comissão? se sim digite a porcentagem, se não digite 0"))
 
-let admissao = prompt ("Informe o ano da sua admissão")
+let admissao = Number (prompt ("Informe o ano da sua admissão"))
 
 
 
+//let idade = Number(prompt("Qual a sua idade?"))
 
 console.log (typeof nome)
 console.log (typeof data)
@@ -36,13 +37,14 @@ console.log (typeof comissao)
 console.log (typeof admissao)
 
 
+console.log("Segue abaixo o Formulário respondido")
 console.log ("Seu nome é ", nome)
 console.log ("Sua data de nascimento é ", data)
 console.log ("Seu endereço é ", end)
 console.log ("O número do seu cpf é ", cpf)
-console.log ("Sua escolaridade é ,", escolaridade)
+console.log ("Sua escolaridade é ", escolaridade)
 console.log ("Você possui cnh? ", cnh)
-console.log ("Você tem ", filhos ,"filhos")
+console.log ("Você tem ", filhos ,"filho(s)")
 console.log ("Você trabalha atualmente como ", cargo)
 console.log ("Seu salário é ", salario , "reais")
 console.log ("Você recebe comissão?", comissao)

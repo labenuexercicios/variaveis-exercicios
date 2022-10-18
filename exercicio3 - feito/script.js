904a5014b702566c4bb6c0688f654f65acd15d7d
HeroZero77 <carenmuraii@gmail.com>
2022-10-18 19:29:56 -0300

    Exercícios completos

---
+++ exercicio3 - feito/script.js
@@ -0,0 +1,38 @@
+/* a) */ const nome =  prompt("Nome completo")
+   
+  /*  b) */  const data = prompt("Data de nascimento (utilize as / )")
+   
+  /*  c) */ let endereço = prompt("Endereço")
+   
+   /* d) */ const cpf = Number(prompt("CPF"))
+   
+   /* e) */ let escolaridade = prompt("Escolaridade")
+   
+   /* f) */ let cnh = prompt("Possui CHN?")
+   
+   /* g) */ let filhos = Number(prompt("Quantos filhos possui?"))
+   
+   /* h) */ let cargo = prompt("Cargo atual")
+   
+   /* i) */ let salario = Number(prompt("Salário"))
+   
+   /* j) */ let comissao =  Number(prompt("Recebe comissão? Se sim, informe a porcentagem, caso não, responda com zero"))
+   
+  /*  k) */ const admissao = Number(prompt("Informe o ano de admissão"))
+
+  console.log(typeof nome, typeof data, typeof endereço, typeof cpf, typeof escolaridade, typeof cnh, typeof filhos, typeof cargo, typeof salario, typeof comissao, typeof admissao)
+  
+  console.log("Nome completo=", nome)
+  console.log("Data de nascimento=", data)
+  console.log("Endereço=", endereço)
+  console.log("CPF=", cpf)
+  console.log("EScolaridade=", escolaridade)
+  console.log("CNH=", cnh)
+  console.log("Filhos=", filhos)
+  console.log("Cargo atual=", cargo)
+  console.log("Salário=", salario)
+  console.log("Comissão=", comissao, "%")
+  console.log("Ano de admissão=", admissao)
+
+
+

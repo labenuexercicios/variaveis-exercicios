fc96da762a7d6b406dc25455d68c73504a994737
bartomsilva <bartomsilva@gmail.com>
2022-10-18 14:03:40 -0300

    feat: criada entrada de dados pelo teclado para nome e idade

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -8,3 +8,16 @@ console.log("idade tipo->",typeof idade)
    sem atribuir um valor.
 */
 
+nome = prompt("Digite seu nome..:")
+idade = Number(prompt("Digite sua idade:"))
+console.log("nome tipo.->",typeof nome)
+console.log("idade tipo->",typeof idade)
+/* como esperado, depois que foi atribuido  
+   valores as variáveis através do prompt, 
+   foi impresso: string e number, sendo que
+   a variável idade teve seu contéudo string
+   que é o padrão do prompt convertido através
+   do Number para o tipo number, contudo
+   uma verificação dos dados digitados se 
+   faz necessária.
+*/

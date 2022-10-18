5b0ec3368e0ca0ec2faf9b25bb7ce29f86b6702f
bartomsilva <bartomsilva@gmail.com>
2022-10-18 13:55:27 -0300

    feat: criadas as variáveis nome e idade

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+let nome 
+let idade 
+
+console.log("nome tipo.->",typeof nome)
+console.log("idade tipo->",typeof idade)
+/* como esperado foi impresso undefined
+   uma vez que as variáveis foram declaradas 
+   sem atribuir um valor.
+*/
+

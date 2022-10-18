0ffa64dce1407e83c2127733393ca552a36ba045
OitaloDev <italobandeiraviana@gmail.com>
2022-10-18 08:36:47 -0300

    Resolução Exercicio 1: Letra A - E

---
+++ exercicio1/script.js
@@ -0,0 +1,17 @@
+let nome = prompt ("Qual seu nome? ");
+let idade = prompt ("Qual sua idade? ");
+
+
+/* Ao executar o comando "typeof" é possivel verificar
+que todos eles estão indefinidos porque não receberam 
+uma atribuição de valor*/
+ 
+
+
+
+
+
+
+
+// prompt = (qual seu nome?)asfasf
+console.log(typeof (nome),typeof (idade));

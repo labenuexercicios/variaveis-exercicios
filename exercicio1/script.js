e02308952c95173a76f08296a8a8c2e72d315839
WeslleiBrito <wesllei.wbs@gmail.com>
2022-10-18 18:15:00 -0300

    respostas a - d

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+let nome
+let idade
+
+console.log("Tipo nome", typeof nome)
+console.log("Tipo idade", typeof idade)
+
+/* Foi impresso este resultado, pois não foi atribuido valor algum a variável */

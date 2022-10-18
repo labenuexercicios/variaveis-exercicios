a2d831e7ca7d778887166e029bb3aa0768665be0
OitaloDev <italobandeiraviana@gmail.com>
2022-10-18 18:51:12 -0300

    Resultado exercicio 18/10

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -12,6 +12,24 @@ uma atribuição de valor*/
 
 
 
+alert ("Olá "+nome+ " Você tem "+idade);
 
-// prompt = (qual seu nome?)asfasf
 console.log(typeof (nome),typeof (idade));
+
+
+let perguntaTexto1 = "Teste pergunta 1"
+let perguntaTexto2 = "Teste pergunta 2"
+let perguntaTexto3 = "Teste pergunta 3"
+
+
+
+
+let pergunta1 = prompt(perguntaTexto1);
+let pergunta2 = prompt(perguntaTexto2);
+let pergunta3 = prompt(perguntaTexto3);
+
+console.log(pergunta1, pergunta2, pergunta3);
+ 
+
+
+

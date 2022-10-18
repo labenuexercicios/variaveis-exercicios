a9039678b3e087fd29c60587f513dbe96b4e81e7
RonyNeves <ronyneves123@gmail,com>
2022-10-18 19:52:28 -0300

    Foi concluido o primeiro exesrcício até a mensagem informando a idade e nome. E foi criado as váriaveis para receber as respostas.

---
+++ exercicio1/script.js
@@ -0,0 +1,19 @@
+let nome
+let idade
+console.log("nome",typeof nome)
+console.log("idade", typeof idade)
+// Ocorreu unddefined por eu não ter declarado o valor
+nome= prompt("Digite seu nome")
+idade= prompt("Digite sua idade")
+
+console.log("nome",typeof nome)
+console.log("idade", typeof idade)
+
+console.log("Olá", nome ,"você tem", idade, "anos")
+
+// Exércício 2 
+
+const resp1= prompt("Você gosta de dinheiro?")
+const resp2= prompt("você gosta de estudar?")
+const resp3= prompt("Você gosta de trabalhar?")
+console.log(resp1,resp2,resp3)

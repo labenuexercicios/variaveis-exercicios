545c29e4ab09b32eae81633e033baf257653cf59
BrunoNMoura <bnmalemao@gmail.com>
2022-10-19 18:56:37 -0300

    Exercícios finalizados

---
+++ exercicio1/script.js
@@ -0,0 +1,24 @@
+// exercíco 1
+const nomeP = "Qual é seu nome?"
+const idadeP = "Qual é sua idade?"
+
+let nomeR = prompt (nomeP)
+let idadeR = Number (prompt (idadeP))
+
+
+const carroP = "Você tem carro?"
+const motoP = "Você tem moto?"
+const bicicletaP = "Você tem bicicleta?"
+
+let carroR = prompt (carroP)
+let motoR = prompt (motoP)
+let bicicletaR = prompt (bicicletaP)
+
+
+console.log ("Seu nome é",nomeR,"você tem",idadeR,"anos")
+console.log("Você tem carro?",carroR)
+console.log("Você tem moto?",motoR)
+console.log("Você tem bicicleta?",bicicletaR)
+
+console.log (typeof nomeR)
+console.log (typeof idadeR)
---
+++ exercicio2/script.js
@@ -0,0 +1,8 @@
+let a = 10
+let b = 25
+let c
+c = a
+a = b
+b = c
+console.log("O valor de a é",a)
+console.log("O valor de b é",b)

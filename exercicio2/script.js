da7b2cb143fc7dc3ff92cdc73ce77057849b2795
Eliel Souza <eliellsouzza@gmail.com>
2022-10-18 21:27:03 -0300

    exercicio2 feito

---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+let a = 18;
+let b = 9
+let c 
+
+console.log ("Valor da variavel \"a\" é:", a + "\n o valor da variavel \"b\" é:", b)
+
+c = a 
+a = b
+b = c
+
+console.log("agora o valor da variavel \a\" é:", a + "\n o valor da variavel \"b\" é:", b)

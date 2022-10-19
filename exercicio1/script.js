a30daddf9227147d4806b56eb577e6ddc88ef61c
AnaGabiM <ana.agma98@gmail.com>
2022-10-18 21:40:03 -0300

    Exercicios

---
+++ exercicio1/script.js
@@ -0,0 +1,31 @@
+/* d- Variaveis com valores indefinidos tambem 
+terao seus tipos indefinidos*/
+
+/*f- Ambos os tipos das variaveis sao strings*/
+
+let nome = prompt("Qual é o seu nome?")
+let idade = prompt("Qual sua idade?")
+
+console.log('Ola', nome, 'voce tem', idade, 'anos')
+
+//parte 2
+
+let pet = ("voce tem pet?")
+const resp1 = prompt (pet)
+let exercicios = ("voce faz exercicios?")
+const resp2 = prompt(exercicios)
+let planta = ("voce tem plantas?")
+const resp3 = prompt (planta)
+
+console.log(pet, resp1)
+console.log(exercicios, resp2)
+console.log(planta, resp3)
+
+
+
+
+
+
+
+
+
---
+++ exercicio2/script.js
@@ -0,0 +1,14 @@
+let a = 10
+let b = 25
+let c = 
+
+console.log("variavel A", a)
+console.log("variavel B", b)
+
+c = a
+a = b
+b = c
+
+console.log("Novo valor de A", a)
+console.log("Novo valor de B", b)
+

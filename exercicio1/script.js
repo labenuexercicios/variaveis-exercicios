0eb6c701bf53fcbe988c92fd32cdb1c476b4146b
Lucasdevbulletproof <[bassolucas1997@gmail.com}>
2022-11-21 20:02:24 -0300

    resoluca-ecercicio1-parte2

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -11,3 +11,22 @@ console.log(typeof (idade))
 console.log("nome", nome)
 console.log("idade", idade)
 console.log("Olá", nome, "voce tem", idade,"anos.")
+
+
+const pergunta1 = "Na sua casa tem net?"
+const pergunta2 = "Qual sua idade?"
+const pergunta3 = "Qual seu time?"
+
+const resposta1 = prompt(pergunta1)
+const resposta2 = prompt(pergunta2)
+const resposta3 = prompt(pergunta3)
+
+console.log(pergunta1,"-", resposta1)
+console.log(pergunta2, "-", resposta2)
+console.log(pergunta3, "-", resposta3)
+
+
+
+
+
+

dc075147bc338ed912d79801d7f08cb4f47b2771
Kenzie <silvadesouzaanalice92@gmail.com>
2022-11-22 12:38:00 -0300

    concluido

---
+++ exercicio2/script.js
@@ -0,0 +1,27 @@
+//Primeira Resolução
+/*
+let minhaIdadeAnoPassado = 19; 
+let minhaIdadeEsseAno = 18;
+
+console.log(`O novo valor de minhaIdadeAnoPassado é ${minhaIdadeEsseAno}`);
+console.log(`O novo valor de minhaIdadeEsseAno é ${minhaIdadeAnoPassado}`);
+*/
+
+let minhaIdadeAnoPassado = 19; 
+let minhaIdadeEsseAno = 18; 
+let minhaIdade;
+
+minhaIdade = minhaIdadeEsseAno;
+minhaIdadeEsseAno = minhaIdadeAnoPassado;
+minhaIdadeAnoPassado = minhaIdade;
+
+console.log(minhaIdadeAnoPassado)
+console.log(minhaIdadeEsseAno)
+console.log(minhaIdade)
+
+
+
+
+
+
+

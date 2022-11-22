192df033d0407d40250fc6e82d36c47170f7086e
Lucasdevbulletproof <[bassolucas1997@gmail.com}>
2022-11-21 22:29:52 -0300

    resolução do exercicio 2

---
+++ exercicio2/script.js
@@ -0,0 +1,16 @@
+let a = "preto"
+let b = "roxo"
+//colocando uma variavel com valor vazio para auxiliar na troca do a para o b.
+let c 
+//console log do A e B para mostrar o valor atual
+console.log("o valor de a é",a)
+console.log("o valor de b é",b)
+// o C assume o valor de A
+c = a;
+// o A assume valor de B
+a = b;
+// o B assume valor de C.
+b = c;
+//Console log do A e B para mostrar o valor atual
+console.log("o valor de a é",a)
+console.log("o valor de b é",b)

a7fb97371af6829b0d77e5bf6796b50c7a31de49
RocioSuarez <suarez.rocio4344@gmail.com>
2022-11-21 23:12:13 -0300

    exercicio-resolvido

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+var mensagem0 = "foi impresso na tela porque o comando console.log imprime no terminal e typeof define o tipo de dados";
+console.log(mensagem0);
+
+var mensagem1 = "qual e seu nome";
+let nome = prompt(mensagem1);
+
+var mensagem2 ="oi "+ nome + " qual e sua idade?";
+let idade = prompt(mensagem2);
+alert(`Óla ${nome}, você tem ${idade} anos`);

1970045264731247c15f5fa9ea4bf26b27960135
astron4ut <ctt.astro@gmail.com>
2022-12-01 22:23:04 -0300

    resolução dos exercícios

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+let nomeDoAluno = "Roberto Santos";
+let idadeDoAluno = 16;
+
+let notaProva1 = 6
+let notaProva2 = 7.5
+let notaProva3 = 8
+
+let mediaFinal = (notaProva1 + notaProva2 + notaProva3) / 3;
+
---
+++ exercicio2/script.js
@@ -0,0 +1,8 @@
+var usuarioTemPet = prompt("Você tem pet?");
+console.log(usuarioTemPet);
+
+let nomeDoPet = prompt("Qual o nome do seu pet?");
+console.log(nomeDoPet);
+
+let idadeDoPet = prompt("Qual a idade do seu pet?");
+console.log(idadeDoPet);

08246251358e241b4261ded137928c99756bfe49
isaiasdevs <montenegro21@gmail.com>
2022-12-18 00:12:11 -0300

    praticando

---
+++ exercicio1/script.js
@@ -0,0 +1,31 @@
+console.log("Parte 1")
+console.log("  ")
+
+// const nome = " "
+// let idade = " "
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+
+// // a variavel idade está definida como uma string que vem em definição por default
+
+
+const nome = prompt("qual seu nome?")
+let idade = prompt("Qual sua idade? ")
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+// a variavel idade está definida como uma string que vem em definição por default
+
+console.log(nome)
+console.log(idade)
+
+
+console.log("Olá!", nome, "você tem", idade, "anos")
+
+
+console.log("===============================================================")
+console.log("Parte 2")
+
+const pergunta1 = prompt("Você possui algum pet?\n Digite 1 - sim ou 2 - não")

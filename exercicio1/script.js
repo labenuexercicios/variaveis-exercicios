483043408536fe86af65b65111c0cb9ca2396176
isaiasdevs <montenegro21@gmail.com>
2022-12-18 15:14:53 -0300

    concluido

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -28,4 +28,23 @@ console.log("Olá!", nome, "você tem", idade, "anos")
 console.log("===============================================================")
 console.log("Parte 2")
 
-const pergunta1 = prompt("Você possui algum pet?\n Digite 1 - sim ou 2 - não")
+// const pergunta1 = prompt("Você tem mais de 18 anos?")
+// const pergunta2 = prompt("Você já viajou para outro estado?")
+// const pergunta3 = prompt("você gosta de cafe?")
+
+// console.log("Primeira resposta:", pergunta1)
+// console.log("Segunda resposta:", pergunta2)
+// console.log("Terceira resposta:", pergunta3)
+
+
+const pergunta1 = "Você tem mais de 18 anos?"
+const pergunta2 = "Você já viajou para outro estado?"
+const pergunta3 = "você gosta de cafe?"
+
+ resposta1 = prompt(pergunta1)
+ resposta2=prompt(pergunta2)
+ resposta3=prompt(pergunta3)
+
+console.log(pergunta1, resposta1)
+console.log(pergunta2, resposta2)
+console.log(pergunta3, resposta3)

d83fe6e9bbfcb48008de2b29b66e42cc5593bad1
henriquelbk <loubackhenrique123@gmail.com>
2023-01-31 21:03:08 -0300

    exercícios de variáveis

---
+++ exercicio1/script.js
@@ -0,0 +1,68 @@
+// PARTE 1
+
+// a)
+
+//let nome 
+
+// b)
+
+//let idade 
+
+// c) e d) 
+
+//console.log(typeof nome)
+//console.log(typeof idade)
+
+/*
+Ambas são undefined
+*/
+
+// e)
+
+//nome = prompt('qual o seu nome?')
+//idade = prompt('qual a sua idade?')
+
+//console.log(nome)
+//console.log(idade)
+
+// f)
+
+//console.log(typeof nome)
+//console.log(typeof idade)
+/*
+Os tipos das variáveis se tornaram strings, pois é o que o prompt retorna
+*/
+
+// g)
+
+//console.log('Olá', nome, 'você tem', idade, 'anos')
+
+// PARTE 2
+
+// a)
+
+//let a = prompt('Qual o seu time?')
+//let b = prompt('Que time é teu?')
+//let c = prompt('fala tu?')
+
+// b)
+
+//console.log(a, b, c)
+
+// c)
+
+let x = 'Qual o seu time?'
+let y = 'Que time é teu?'
+let z = 'Fala tu?'
+
+// d)
+
+let a = prompt(x)
+let b = prompt(y)
+let c = prompt(z)
+
+//console.log(a, b, c)
+
+// e)
+
+console.log(x, y, z, a, b, c)
---
+++ exercicio3/script.js
@@ -0,0 +1,15 @@
+const nomeCompleto = prompt('Nome Completo:')
+const dataDeNascimento = prompt('Data de Nascimento: (Utiliza as /)')
+let endereço = prompt('Endereço:')
+const cpf = Number(prompt('CPF:'))
+let escolaridade = prompt('Escolaridade:')
+let cnh = prompt('Possui CNH?')
+let filhos = Number(prompt('Quantos filhos possui?'))
+let cargo = prompt('Cargo:')
+let salario = Number(prompt('Salário:'))
+let comissao = Number(prompt('Recebe comissão? Se sim, informe a porcentagem, caso não, responda com zero'))
+const admissao = Number(prompt('Informe ano de admissão:'))
+
+console.log(typeof(nomeCompleto, dataDeNascimento, endereço, cpf, escolaridade, cnh, filhos, cargo, salario, comissao, admissao))
+console.log(nomeCompleto, dataDeNascimento, endereço, cpf, escolaridade, cnh, filhos, cargo, salario, comissao, admissao)
+

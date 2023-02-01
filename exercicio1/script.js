6d9f226fc3863894b26372948a2f3f23f7248002
imlaerciojunior <imlaerciojunior@gmail.com>
2023-02-01 18:49:27 -0300

    Respostas dos Exercicios de variaveis

---
+++ exercicio1/script.js
@@ -0,0 +1,17 @@
+// EXERCICIO 1 - PARTE 1
+
+// let nome 
+// let idade 
+// console.log(typeof nome, idade)
+
+// não foi impresso porque os valores não foram 
+//atribuidos as variáveis 
+
+// nome = prompt("Qual é seu nome?")
+// idade = Number(prompt("Qual é sua idade?"))
+
+// console.log(typeof nome, idade)
+
+// console.log("Ola,", nome, "você tem", idade, "anos")
+
+
---
+++ exercicio2/script.js
@@ -0,0 +1,17 @@
+// EXERCÍCIO 2
+
+// let a = 10
+// let b = 25
+// let c
+
+// guardando valor A dentro de C
+// c = a
+// console.log('C:', c)
+
+// guardando valor B dentro de A
+// a = b
+// console.log('A:', a)
+
+// guardando o valor de C dentro de B
+// b = c
+// console.log('B:', b)

8228415434a249b66359ca1cac43c82cda0d61e7
ramon-valvassori <ramonepico@gmail.com>
2023-02-01 18:27:32 -0300

    Parte de exercícios

---
+++ exercicio1/script.js
@@ -0,0 +1,16 @@
+let nome
+let idade
+let altura
+
+console.log(typeof (nome), typeof (idade))
+
+//O código não foi impresso pois as variáveis não estão definidas
+
+console.log("Olá", nome,  "você tem", idade, "anos")
+
+nome = prompt("Qual seu nome", nome,"?")
+idade = Number (prompt("Qual sua idade", idade,"?"))
+console.log(typeof nome, typeof idade)
+
+
+

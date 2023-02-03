2527d3b2a867c8df7dae204d812b32fd63f4f565
carolinabispo <carolinabispos07@gmail.com>
2023-02-02 22:15:10 -0300

    exercicios de variaveis

---
+++ exercicio1/script.js
@@ -0,0 +1,39 @@
+//let nome = 
+//console.log(typeof nome)
+//let idade =  
+//console.log(typeof idade)
+
+//as variáveis tem o tipo de undefined pois não foi atribuido valor a elas
+
+//let nome = prompt('qual o seu nome?')
+//let idade = prompt('qual a sua idade?')
+//console.log(typeof nome)
+//console.log(typeof idade)
+
+//foi notado que tanto a variável nome e idade tiveram o tipo de string
+
+//console.log(`olá  ${nome}, você tem ${idade} anos.`)
+
+
+//PARTE 2
+const jogo = 'você gosta de jogar?'
+const sentado = 'você está sentado?'
+const viagem = 'você gosta de viajar?'
+//console.log( jogo, sentado, viagem )
+
+let respostaA = prompt(jogo)
+let respostaB = prompt(sentado)
+let respostaC = prompt(viagem)
+
+console.log(`${jogo} ${respostaA} \n ${sentado} ${respostaB} \n ${viagem} ${respostaC}`)
+
+
+
+
+
+
+
+
+
+
+

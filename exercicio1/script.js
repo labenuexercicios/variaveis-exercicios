4cba101f6b0472a9ac35c27996926861ff449e5e
bandreid01 <viniciusdasilvax@gmail.com>
2023-03-15 08:41:08 -0300

    Resolução Exercicios Parte 1

---
+++ exercicio1/script.js
@@ -0,0 +1,16 @@
+//const nome = null 
+//let idade = null
+//  Foi impresso object pois foi intensionalmente inicializado vazio
+
+
+const nome = prompt('Qual seu nome? :')
+
+let idade = prompt('Qual sua idade? :')
+//nome = "Vinicius"
+//idade = 32
+console.log(typeof idade, typeof nome)
+// as 2 var tornaram-se strings
+
+
+console.log("Olá ",nome," você tem ",idade," anos")
+//console.log(typeof idade, typeof nome)

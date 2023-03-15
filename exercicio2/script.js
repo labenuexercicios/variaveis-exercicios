4f6ab371c922bc962e45562fd7eed3d6beae4bca
johndesk2205 <joaomarcos2205@gmail.com>
2023-03-15 20:03:55 -0300

    Resolucoes dos exercicios nos arquivos scripts.js

---
+++ exercicio2/script.js
@@ -0,0 +1,17 @@
+let a = 10
+
+let b = 30
+
+console.log("Valor de a:", a)
+console.log("Valor de b:", b);
+
+let c
+
+c = a
+
+a = b
+
+b = c
+
+console.log("O novo valor de a é:", a)
+console.log("O novo valor de b é:", b)

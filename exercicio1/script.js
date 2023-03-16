8e3cf0f89466d47e56644e6ed236ea0f43f172ea
LarissaPinheiro <lps.pinheiro1@gmail.com>
2023-03-16 07:58:46 -0400

    Exercicios de variáveis

---
+++ exercicio1/script.js
@@ -0,0 +1,38 @@
+// Parte 1
+
+// let nome = null
+// let idade = null
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+// objeto indefinido 
+
+const Nome = prompt("Qual seu nome?")
+const Idade = Number (prompt("Qual sua idade?"))
+
+console.log(typeof Nome)
+console.log(typeof Idade)
+
+console.log ("Olá!" , Nome,  "você tem", Idade , "anos")
+
+// PArte 2
+
+const perguntaA = 'Você tem pet?'
+let resA = prompt(perguntaA)
+console.log(perguntaA, resA)
+
+const perguntaB = "Qual seu suco favorito?"
+let resB = prompt(perguntaB)
+console.log(perguntaB, resB)
+
+const perguntaC = "Na sua cidade chove muito?"
+let resC = prompt(perguntaC)
+console.log(perguntaC, resC)
+
+
+
+
+
+
+
---
+++ exercicio2/script.js
@@ -0,0 +1,13 @@
+let a = 10
+console.log("a",a)
+let b = 25
+console.log("b",b)
+let c 
+console.log("c",c)
+
+c = b 
+console.log("c",c)
+b = a
+console.log("b",b)
+a = c
+console.log("a",a)

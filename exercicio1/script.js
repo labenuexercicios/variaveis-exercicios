a141f0ab92f6b03469aa3eed2193d05407f25026
brdresch <brdresch@gmail.com>
2023-07-11 18:51:30 -0300

    Exercício1

---
+++ exercicio1/script.js
@@ -0,0 +1,15 @@
+const userName = prompt("Qual seu nome?")
+
+console.log(userName)
+
+console.log(typeof userName)
+
+let idade = Number (prompt("Qual a sua idade?"))
+
+console.log(idade)
+
+console.log(typeof idade)
+
+//**g)** Para finalizar, imprima na tela a mensagem: "Olá, `nome`,  você tem `idade` anos". Lembre-se: `nome` e `idade` são os valores inseridos pelo usuário.
+
+console.log("Olá ", userName, ",sua idade é ", idade,"anos.")

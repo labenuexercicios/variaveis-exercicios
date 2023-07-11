fb91341689cfc8dd68ab03e37ab35a41f762e8b2
Gilson Sebastião <gilsoncbsjr@gmail.com>
2023-07-11 19:03:06 -0300

    Exercicio 2 feito

---
+++ exercicio2/script.js
@@ -0,0 +1,16 @@
+//const perguntaA = prompt("Você é maior de idade?")
+//const perguntaB = prompt("Você é brasileiro?")
+//const perguntaC = prompt("Você curte One Piece?")
+//console.log(perguntaA, perguntaB, perguntaC);
+
+const perguntaA = "Você é maior de idade?"
+const respostaA = prompt(perguntaA)
+console.log(perguntaA, respostaA);
+
+const perguntaB = "Você é brasileiro?"
+const respostaB = prompt(perguntaB)
+console.log(perguntaB, respostaB);
+
+const perguntaC = "Você curte One Piece?"
+const respostaC = prompt(perguntaC)
+console.log(perguntaC, respostaC);

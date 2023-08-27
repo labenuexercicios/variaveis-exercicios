dd9132d3f4074e2f239997f9369f687ed94a0ab0
Leonardo0304alves <leonardoalves.soareso90@gmail.com>
2023-08-27 15:56:31 -0300

    Baker-Leonardo-Alves

---
+++ exercicio1/script.js
@@ -0,0 +1,16 @@
+/*const nome = 'Leonardo'
+let idade = 33
+
+console.log(typeof nome) //typeof é para imprimir o tipo de cada varíavel// 
+console.log(typeof idade)*/
+
+let nome = prompt ('nome')
+const idade = Number( prompt ('idade'))
+
+console.log (nome)
+console. log (idade)
+
+console.log (typeof nome)
+console.log (typeof idade)
+
+/*console.log (`Olá'${nome} , 'você tem' ${idade} 'anos') //esta ocorrendo um erro na hora da impressão*/
